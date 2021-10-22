e3096a7db41437fa235aaf808d7ac3eda8b0786b
William Boyles <williamboyles22900@gmail.com>
2021-10-22 16:53:17 -0400

    Add method to determine parabolas for given nullities

--- code/min_clicks.py
+++ code/min_clicks.py
@@ -1,33 +1,12 @@
 import numpy as np
 
-from board_math import kernel
 from region_separator import regions, region_transform
+from kernel_size import nullity
 
-from pulp import LpProblem, LpMaximize, LpVariable, lpSum
-
-
-def brute_force_min_clicks(n: int) -> int:
-    """
-    Attempts to find the maximum number of clicks needed to optimally solve any n x n board via brute force.
-
-    This algorithm takes O(2^(n^2)) time.
-    """
-
-    # Generate all possible boards
-    all_boards = [
-        np.array([int(c) for c in "{0:b}".format(i).zfill(n ** 2)], dtype=int)
-        for i in range(2 ** (n ** 2))
-    ]
-
-    # Get all quiet patterns
-    quiets = kernel(n)
-
-    # Find the minimum number of clicks equivalent to every board
-    # Return to maximum of these minimum number of clicks
-    return max(min(sum(board ^ quiet) for quiet in quiets) for board in all_boards)
+from pulp import LpProblem, LpMaximize, LpVariable, lpSum, PULP_CBC_CMD
 
 
-def lp_min_clicks(n: int) -> LpProblem:
+def min_clicks_lp(n: int) -> LpProblem:
     """
     Creates an integer program to find the minimum number of clicks needed to optimally solve any n x n board.
     """
@@ -60,3 +39,75 @@ def lp_min_clicks(n: int) -> LpProblem:
         prob += vars[i] <= len(squares)
 
     return prob
+
+
+def min_clicks_parabola(m: int, msg=0) -> np.ndarray:
+    """
+    This algorithm relies on the following conjecture:
+
+    1. Let M(n) give the minimum board size with nullity d(n) such that n is equivalent to -1 mod (M(n) + 1).
+       So, n = (M(n) + 1)*k - 1 for some natural k.
+       Then the minimum number of clicks needed to solve n x n board is some parabola a*k^2 + b*k + c.
+
+    This algorithm finds the parabola a*k^2 + b*k + c for a given nullity m.
+    Note that its's possible for different "families" of parabolas to have the same nullity.
+    M(9) = 9, and d(9) = 8.
+    M(16) = 16, and d(16) = 8.
+    However, 16 is not equivalent to -1 mod (M(9) + 1).
+    """
+
+    if m <= 0:
+        raise ValueError("m must be positive")
+    if m % 2 != 0:
+        raise ValueError("m must be even")
+
+    # Find the smallest board with nullity m
+    # Can start n = m because we know d(n) <= n.
+    least_nullity_m = m
+    while nullity(least_nullity_m) != m:
+        least_nullity_m += 1
+
+    # Find the next two boards with nullity m
+    # We increment by least_nullity_m + 1 by conjecture 1
+    first_three_nullity_m = [(1, least_nullity_m)]
+    k = 2
+    while len(first_three_nullity_m) < 3:
+        n = (least_nullity_m + 1) * k - 1
+        if nullity(n) == m:
+            first_three_nullity_m.append((k, n))
+        k += 1
+
+    # For each k in first_three_nullity, generate a row [k^2, k, 1]
+    A = np.array([[k ** 2, k, 1] for k, _ in first_three_nullity_m])
+
+    # Solve the min moves problem for these three boards
+    min_moves = np.zeros(3, dtype=int)
+    for i, (_, n) in enumerate(first_three_nullity_m):
+        prob = min_clicks_lp(n)
+        prob.solve(PULP_CBC_CMD(msg=msg))
+        min_moves[i] = prob.objective.value()
+
+    # Solve A*x = min_moves for x
+    x = np.linalg.solve(A, min_moves)
+
+    return x
+
+
+"""
+Min Clicks Parabolas
+
+Right column seems to contain numbers: https://oeis.org/A118142
+Nullity | Parabola (a,b,c) | Smallest Board
+--------+------------------+---------------
+2       | (26,-12,1)       | 5
+4       | (17,-10,0)       | 4
+6       | (88,-24,1)       | 11
+8       | (60,-20,-3)      | 9
+8*      | (161,-34,-3)     | 16
+10      | (506,-60,-3)     | 29
+12      | ?                | 84
+14      | ?                | 23
+16      | ?                | 19
+18      | ?                | 101
+20      | ?                | 30 
+"""
