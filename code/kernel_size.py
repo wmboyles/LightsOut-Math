5a6ebdf0b29cbe35160ad48d795f554bf6a3eb86
William Boyles <williamboyles22900@gmail.com>
2022-03-12 01:37:02 -0500

    Clean up polynomials; improve nullity algorithm

--- code/kernel_size.py
+++ code/kernel_size.py
@@ -1,5 +1,82 @@
 from functools import cache
-from polynomials import GF2Polynomial, chebyshev_pair, find_gbk
+from polynomials import GF2Polynomial
+
+
+def find_bk(n: int) -> tuple[int, int]:
+    """
+    Calculates n = b*2^(k-1) - 1, where b and k are naturals and b is odd.
+
+    Raises:
+        ValueError: If n <= 0
+    """
+
+    if n <= 0:
+        raise ValueError("n must be positive")
+
+    binary_n = bin(n + 1)
+    k = len(binary_n) - len(binary_n.rstrip("0")) + 1
+    b = (n + 1) >> (k - 1)
+
+    return b, k
+
+
+@cache
+def f_pair(n: int) -> tuple[GF2Polynomial, GF2Polynomial]:
+    """
+    Recursively define the following polynomials over Z_2[x]
+    f(0,x) = 1, f(1,x) = x
+    f(n+1,x) = x*f(n,x) + f(n-1,x)
+    This method gives f(n,x) and f(n,x+1)
+
+    Raises:
+        ValueError: if n < 0
+    """
+
+    if n < 0:
+        raise ValueError("n must be positive")
+    # f(0,x) = f(0,x+1) = 1
+    elif n == 0:
+        return GF2Polynomial({0}), GF2Polynomial({0})
+
+    """
+    From Hunziker, Machivelo, and Park
+    "Chebyshev Polynomials Over Finite Fields and Reversibility of Sigma-automata on Square Grids"
+    Lemma 2.6 (restated in our notation to avoid confusing offset)
+    Let n = b*2^(k-1) - 1, where b is odd
+    f(n, x)   = f(2^(k-1) - 1, x) * f(b-1, x) ** (2^(k-1))
+              = x^(2^(k-1) - 1)   * f(b-1, x) ** (2^(k-1))
+    """
+
+    b, k = find_bk(n)
+
+    @cache
+    def brute_f1(y: int):
+        """
+        Calculate f(y), where y is even.
+        Hunziker, Machivelo, and Park tell us that the results will be the square of a square-free polynomial.
+        However, they don't give any neat identities here to reduce the problem instance size.
+
+        So, we have to use the relationship between f and binomial coefficients.
+        Sutner tells us that f_y(x) = sum_{i=0}^{y}{C(y+1+i, 2i+1) x^i mod 2}, where C(n,m) = n choose m.
+        Thus, we need to find when C(y+1+i, 2i+1) is odd.
+        Kummer's Theorem tells us that the largest q such that 2^q divides C(n,m) is the number of carries when adding (n-m) and m in base q.
+        If the number of carries is 0 (i.e. (n-m) & m == 0), then C(n,m) is odd.
+        So, C(n+1+i, 2i+1) is odd when (y-i) & (2i+1) == 0.
+        """
+
+        return GF2Polynomial({i for i in range(y + 1) if not ((y - i) & (2 * i + 1))})
+
+    polyb_f1 = brute_f1(b - 1)
+    if k == 1:
+        f1 = polyb_f1
+    else:
+        exp = 2 ** (k - 1)
+        f1 = GF2Polynomial({exp - 1}) * (polyb_f1 ** exp)
+
+    # Calculate f(n,x+1) by evaluating f(n,x) at x+1
+    f2 = f1 @ GF2Polynomial({0, 1})
+
+    return f1, f2
 
 
 @cache
@@ -15,23 +92,43 @@ def nullity(n: int) -> int:
     Then the nullity is equal to the degree of gcd(f(n,x), f(n,1+x)).
     """
 
-    """
-    We proved Sutner's conjecture that d(2n+1) = 2*d(n) + delta_n, and delta_{2n+1} = delta_n.
-    Thus, if n = b*2^(k-1) - 1 where k > 2, it's cheaper to calculate delta_n = nullity(2b - 1) - nullity(b-1).
-    """
-
-    b, k = find_gbk(n)
-
+    # This is the method we'll use if we can't do any tricks
     def brute_nullity(m: int) -> int:
-        return GF2Polynomial.gcd(*chebyshev_pair(m)).degree
+        return GF2Polynomial.gcd(*f_pair(m)).degree
 
-    # A result from Hunziker, Machivelo, and Park and possibly also Sutner
-    # says that d(2^(k-1) - 1) = 0 for all k.
+    # n=0 and n=1 are nice base cases to just have
+    if n == 0 or n == 1:
+        return 0
+
+    # Applying a result from Mazakazu Yamagishi's paper:
+    # "Elliptic Curves Over Finite Fields and Reversibility of Additive Cellular Automata on Square Grids"
+    # in the journal Finite Fields and Their Applications, we find that
+    # d(2^k) = d(2^k - 2) when k is odd and d(2^k - 2) + 4 when k is even.
+
+    # If n is a power of 2... (> 1 condition taken care of above)
+    if n & (n - 1) == 0:
+        # n = 2^k, n.bit_length() = k+1
+        return nullity(n - 2) + (4 if n.bit_length() & 1 else 0)
+
+    # We proved:
+    # 1. d(2n+1) = 2*d(n) + delta_n
+    # 2. delta_{2n+1} = delta_n.
+    # 3. delta_n = 2 * deg gcd(x, f_n(x+1) / g), where g = gcd(f_n(x), f_n(x+1)).
+    # Thus, we'll take advantage of this to speed up our answer for n = b*2^(k-1) - 1 where k is large
+
+    b, k = find_bk(n)
+
+    # Hunziker, Machivelo, and Park and possibly also Sutner say  d(2^k - 1) = 0.
     if b == 1:
         return 0
-    elif k > 2:
-        a1, a2 = brute_nullity(b - 1), brute_nullity(2 * b - 1)
-        delta = a2 - 2 * a1
-        return (a1 + delta) * 2 ** (k - 1) - delta
 
-    return brute_nullity(n)
+    fp = f_pair(b - 1)
+    g = GF2Polynomial.gcd(*fp)
+    a = g.degree
+
+    # k=1 means we had to brute force: calculating te gcd of f_n(x) and f_n(x+1)
+    if k == 1:
+        return a
+    else:
+        delta = 2 * GF2Polynomial.gcd(GF2Polynomial({1}), fp[1] // g).degree
+        return (a + delta) * 2 ** (k - 1) - delta
--- code/polynomials.py
+++ code/polynomials.py
@@ -1,20 +1,28 @@
 from __future__ import annotations
 
 from dataclasses import dataclass, field
-from functools import cache, reduce, cached_property
+from functools import reduce, cached_property
 
 
 @dataclass(repr=False, eq=True, frozen=True)
 class GF2Polynomial:
     """
     Represents polynomials in Z_2[x].
-    Implements operations that make sense in this field.
+    Implements operations that make sense in this ring.
 
     Args:
         degrees (set[int]): Set of integers representing degrees of polynomial. For example, __init__({2,0}) = x^2 + 1.
     """
 
     degrees: set[int] = field(default_factory=set)
+
+    @property
+    def is_zero(self) -> bool:
+        """
+        Checks if polynomial is the constant function 0.
+        """
+
+        return not self.degrees
 
     @cached_property
     def degree(self) -> int:
@@ -23,14 +31,14 @@ class GF2Polynomial:
         For example, x^2 + 1 has degree 2.
         """
 
-        return 0 if not self.degrees else max(self.degrees)
+        return 0 if self.is_zero else max(self.degrees)
 
     def __str__(self) -> str:
         """
         Print polynomial in written form, like x^2 + x^1 + x^0.
         """
 
-        return " + ".join(f"x^{n}" for n in self.degrees) if self.degrees else "0"
+        return "0" if self.is_zero else " + ".join(f"x^{n}" for n in self.degrees)
 
     def __repr__(self) -> str:
         return self.__str__()
@@ -78,13 +86,6 @@ class GF2Polynomial:
             GF2Polynomial(),
         )
 
-    def _is_zero(self) -> bool:
-        """
-        Checks if polynomial is the constant function 0.
-        """
-
-        return not self.degrees
-
     def __divmod__(self, div: GF2Polynomial) -> tuple[GF2Polynomial, GF2Polynomial]:
         """
         Compute the floor quotient and remainder.
@@ -93,7 +94,7 @@ class GF2Polynomial:
         # Remainder and quotient
         r = GF2Polynomial(self.degrees)
         q = GF2Polynomial()
-        while (d_deg := r.degree - div.degree) >= 0 and not r._is_zero():
+        while (d_deg := r.degree - div.degree) >= 0 and not r.is_zero:
             # x^(d_deg) gives next term
             q += GF2Polynomial({d_deg})
 
@@ -118,15 +119,6 @@ class GF2Polynomial:
 
         return self.__divmod__(mod)[1]
 
-    def __distributive_pow(self, n: int) -> GF2Polynomial:
-        """
-        If self is x^a + x^b + ..., then this method returns x^(an) + x^(bn) + ... .
-        Since (x^a + x^b)^n =  x^(an) + x^(bn) + ... when n is a power of 2,
-        this method can help compute exponentiation usually faster than exponentiation by squaring.
-        """
-
-        return GF2Polynomial({n * deg for deg in self.degrees})
-
     def __pow__(self, exp: int, mod: GF2Polynomial = None) -> GF2Polynomial:
         """
         Compute polynomial to some non-negative integer power, possibly modulo some polynomial.
@@ -137,29 +129,45 @@ class GF2Polynomial:
             ZeroDivisionError: If mod is 0
         """
 
+        def distributive_pow(n: int) -> GF2Polynomial:
+            """
+            If self is x^a + x^b + ..., then this method returns x^(an) + x^(bn) + ... .
+            Since (x^a + x^b)^n =  x^(an) + x^(bn) + ... when n is a power of 2,
+            this method can help compute exponentiation usually faster than exponentiation by squaring.
+            """
+
+            return GF2Polynomial({n * deg for deg in self.degrees})
+
         if exp < 0:
             raise ValueError("Exponent cannot be negative")
-        if mod and mod._is_zero():
+        if mod and mod.is_zero:
             raise ZeroDivisionError("Modulus cannot be 0")
 
+        # (x^a ... + x^k)^i = (x^a ... + x^k)^(j1) * (x^a ... + x^k)^(j2) * ... * (x^a ... + x^k)^(jn)
+        # where j1, j2, ..., jn are the powers of 2 that sum to i (i.e. i's representation in binary).
         # If i is a power of 2, then (x^a ... + x^k)^i = x^(ai) + ... + x^(ki)
-        i = 1
+        # So, we can precompute each (x^a ... + x^k)^j1, j2, ..., jn and multiply them together.
+        # This is at least as fast as exponentiation by squaring b/c we have to do O(log2(i)) brute force multiplications.
         poly_list = list[GF2Polynomial]()
+        i = 1
         while i <= exp:
             if i & exp:
-                poly_list.append(self.__distributive_pow(i))
+                poly_list.append(distributive_pow(i))
             i <<= 1
 
         prod = GF2Polynomial({0})
         for poly in poly_list:
-            prod = (prod * poly) if not mod else (prod * poly) % mod
+            prod = prod * poly
+
+            if mod:
+                prod %= mod
 
         return prod
 
     def __matmul__(self, g: GF2Polynomial) -> GF2Polynomial:
         """
         Let f(x) = self, g(x) = g.
-        Then this method returns f(g(x)).
+        Then this method returns f(g(x)), the composition of f and g.
         """
 
         return reduce(
@@ -174,85 +182,7 @@ class GF2Polynomial:
         Compute the greatest common division of two polynomials
         """
 
-        while not g._is_zero():
+        while not g.is_zero:
             f, g = g, f % g
 
         return f
-
-
-def find_gbk(n: int) -> tuple[int, int]:
-    """
-    Cakculates n = b*2^(k-1) - 1, where b and k are naturals and b is odd.
-
-    Raises:
-        ValueError: If n <= 0
-    """
-
-    if n <= 0:
-        raise ValueError("n must be positive")
-
-    binary_n = bin(n + 1)
-    b_str = binary_n.rstrip("0")
-    k = len(binary_n) - len(b_str) + 1
-    b = int(b_str, 2)
-
-    return b, k
-
-
-@cache
-def chebyshev_pair(n: int) -> tuple[GF2Polynomial, GF2Polynomial]:
-    """
-    Recursively define the following polynomials over Z_2[x]
-    f(0,x) = 1, f(1,x) = x
-    f(n+1,x) = x*f(n,x) + f(n-1,x)
-    This method gives f(n,x) and f(n,x+1)
-
-    Raises:
-        ValueError: if n < 0
-    """
-
-    if n < 0:
-        raise ValueError("n must be positive")
-
-    # f(0,x) = f(0,x+1) = 1
-    elif n == 0:
-        return GF2Polynomial({0}), GF2Polynomial({0})
-
-    """
-    From Hunziker, Machivelo, and Park
-    "Chebyshev Polynomials Over Finite Fields and Reversibility of Sigma-automata on Square Grids"
-    Lemma 2.6 (restated in our notation to avoid confusing offset)
-    Let n = b*2^(k-1) - 1, where b is odd
-    f(n, x)   = f(2^(k-1) - 1, x) * f(b-1, x) ** (2^(k-1))
-              = x^(2^(k-1) - 1)   * f(b-1, x) ** (2^(k-1))
-    """
-    b, k = find_gbk(n)
-
-    @cache
-    def brute_f1(y: int):
-        """
-        Calculate f(y), where y is even.
-        Hunziker, Machivelo, and Park tell us that the results will be the square of a square-free polynomial.
-        However, they don't give any neat identities here to reduce the problem instance size.
-
-        So, we have to use the relationship between f and binomial coefficients.
-        Sutner tells us that f_y(x) = sum_{i=0}^{y}{C(y+1+i, 2i+1) x^i mod 2}.
-        Thus, we need to find when C(y+1+i, 2i+1) is odd.
-        Kummer's theorem, tells us that the largest q such that 2^q divides C(n,m) is the number of carries when adding (n-m) and m in base q.
-        If the number of carries is 0 (i.e. (n-m) & m == 0), then C(n,m) is odd.
-        So, C(n+1+i, 2i+1) is odd when (y-i) & (2i+1) == 0.
-        """
-
-        return GF2Polynomial({i for i in range(y + 1) if not ((y - i) & (2 * i + 1))})
-
-    polyb_f1 = brute_f1(b - 1)
-    if k == 1:
-        f1 = polyb_f1
-    else:
-        exp = 2 ** (k - 1)
-        f1 = GF2Polynomial({exp - 1}) * (polyb_f1 ** exp)
-
-    # Calculate f(n,x+1) by evaluating f(n,x) at x+1
-    f2 = f1 @ GF2Polynomial({0, 1})
-
-    return f1, f2
