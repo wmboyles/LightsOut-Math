4ae9ef26b98c6b5789d7908a0f6be73337d5f3e5
William Boyles <wmboyles@users.noreply.github.com>
2026-09-12 15:40:59 -0700

    Standardize terms

--- tex/nullity2/nullity2.lean
+++ tex/nullity2/nullity2.lean
@@ -3,7 +3,7 @@ import Mathlib.Data.ZMod.Basic
 import Mathlib.Combinatorics.SimpleGraph.Basic
 import Mathlib.LinearAlgebra.FiniteDimensional.Basic
 
--- State of a graph: Which verticies are clicked or on, depending on context
+-- State of a graph: Which verticies are pressed or on, depending on context
 abbrev State (V : Type*) := Finset V
 
 -- A vertex v and all its neighbors
@@ -14,7 +14,7 @@ def closedNeighborhood
   : State V
   := Finset.univ.filter (fun u => u = v ∨ G.Adj v u)
 
--- Given an initial state and one clicked vertex, give the final state
+-- Given an initial state and one pressed vertex, give the final state
 def press
   {V : Type*} [Fintype V] [DecidableEq V]
   (G : SimpleGraph V) [DecidableRel G.Adj]
@@ -69,8 +69,8 @@ def pressedSet
   | []      => ∅
   | v :: vs => symmDiff {v} (pressedSet vs)
 
--- Whether a vertex v was clicked, given a set of clicked vertices
-def clickedValue
+-- Whether a vertex v was pressed, given a set of pressed vertices
+def pressedValue
   {V : Type*} [DecidableEq V]
   (S : State V)
   (v : V)
@@ -86,31 +86,31 @@ lemma zmod2_cases
     · exact Or.inl rfl
     · exact Or.inr rfl
 
-/- clickedValue is linear with respect to symmetric difference
-clickedValue (S1 ∆ S2) v = (clickedValue S1 v) ∆ (clickedValue S2 v)
+/- pressedValue is linear with respect to symmetric difference
+pressedValue (S1 ∆ S2) v = (pressedValue S1 v) ∆ (pressedValue S2 v)
 -/
-lemma clickedValue_symmDiff
+lemma pressedValue_symmDiff
   {V : Type*} [DecidableEq V]
   (S1 S2 : State V)
   (v : V)
-  : clickedValue (symmDiff S1 S2) v = clickedValue S1 v + clickedValue S2 v
+  : pressedValue (symmDiff S1 S2) v = pressedValue S1 v + pressedValue S2 v
   := by
     -- 1 + 1 = 0 mod 2
     have h : (1 : ZMod 2) + 1 = 0 := by decide
     by_cases h1 : v ∈ S1 <;>
       by_cases h2 : v ∈ S2 <;>
-        simp [clickedValue, Finset.symmDiff_def, h1, h2, h]
+        simp [pressedValue, Finset.symmDiff_def, h1, h2, h]
 
-/- Whether a vertex v changes state after the vertices in S are clicked.
+/- Whether a vertex v changes state after the vertices in S are pressed.
 Vertex v changes state exactly when and odd number of vertices
-in its closed neighborhood are clicked.
+in its closed neighborhood are pressed.
 -/
 def changedValue
   {V : Type*} [Fintype V] [DecidableEq V]
   (G : SimpleGraph V) [DecidableRel G.Adj]
   (S : State V) (v : V)
   : ZMod 2
-  := clickedValue S v + ∑ u, if G.Adj v u then clickedValue S u else 0
+  := pressedValue S v + ∑ u, if G.Adj v u then pressedValue S u else 0
 
 /- changedValue is linear with respect to symmetric difference
 changedValue G (S1 ∆ S2) v = (changedValue G S1 v) ∆ (changedValue G S2 v)
@@ -123,47 +123,47 @@ lemma changedValue_symmDiff
   : changedValue G (symmDiff S1 S2) v = changedValue G S1 v + changedValue G S2 v
   := by
     /- Expand our goal using the definition of changedValue
-    clickedValue (S1 ∆ S2) v + ∑ u, if u~v then clickedValue (S1 ∆ S2) u else 0
-      = (clickedValue S1 v + ∑ u, u~v then clickedValue S1 u else 0)
-      + (clickedValue S2 v + ∑ u, u~v then clickedValue S2 u else 0)
+    pressedValue (S1 ∆ S2) v + ∑ u, if u~v then pressedValue (S1 ∆ S2) u else 0
+      = (pressedValue S1 v + ∑ u, u~v then pressedValue S1 u else 0)
+      + (pressedValue S2 v + ∑ u, u~v then pressedValue S2 u else 0)
     -/
     simp only [changedValue]
-    /- Expand the LHS using clickedValue_symmDiff
-    (clickedValue S1 v) + (clickedValue S2 v)
-      + ∑ u, if u~v then (clickedValue S1 u) + (clickedValue S2 u) else 0
-    = (clickedValue S1 v + ∑ u, if u~v then clickedValue S1 u else 0)
-      + (clickedValue S2 v + ∑ u, if u~v then clickedValue S2 u else 0)
+    /- Expand the LHS using pressedValue_symmDiff
+    (pressedValue S1 v) + (pressedValue S2 v)
+      + ∑ u, if u~v then (pressedValue S1 u) + (pressedValue S2 u) else 0
+    = (pressedValue S1 v + ∑ u, if u~v then pressedValue S1 u else 0)
+      + (pressedValue S2 v + ∑ u, if u~v then pressedValue S2 u else 0)
     -/
-    simp only [clickedValue_symmDiff]
+    simp only [pressedValue_symmDiff]
     -- Now we need to show we can break the sum into two for S1 and S2
     have h :
-      (∑ u, if G.Adj v u then clickedValue S1 u + clickedValue S2 u else 0) =
-        (∑ u, if G.Adj v u then clickedValue S1 u else 0) +
-        (∑ u, if G.Adj v u then clickedValue S2 u else 0)
+      (∑ u, if G.Adj v u then pressedValue S1 u + pressedValue S2 u else 0) =
+        (∑ u, if G.Adj v u then pressedValue S1 u else 0) +
+        (∑ u, if G.Adj v u then pressedValue S2 u else 0)
       := by
       calc
-        (∑ u, if G.Adj v u then clickedValue S1 u + clickedValue S2 u else 0) =
+        (∑ u, if G.Adj v u then pressedValue S1 u + pressedValue S2 u else 0) =
         ∑ u, (
-          (if G.Adj v u then clickedValue S1 u else 0) +
-          (if G.Adj v u then clickedValue S2 u else 0))
+          (if G.Adj v u then pressedValue S1 u else 0) +
+          (if G.Adj v u then pressedValue S2 u else 0))
         := by
           -- We'll show the sums are equal by showing each term is equal
           apply Finset.sum_congr rfl
           -- Let u ∈ V
           intro u hu
-          -- If u~v, then we have clickedValue S1 u + clickedValue S2 u on both sides
+          -- If u~v, then we have pressedValue S1 u + pressedValue S2 u on both sides
           -- Otherwise, we have 0 = 0 + 0, which is also true
           by_cases huv : G.Adj v u <;> simp [huv]
         -- Now we want to split the LHS into two sums
-        _ = (∑ u, if G.Adj v u then clickedValue S1 u else 0) +
-            (∑ u, if G.Adj v u then clickedValue S2 u else 0)
+        _ = (∑ u, if G.Adj v u then pressedValue S1 u else 0) +
+            (∑ u, if G.Adj v u then pressedValue S2 u else 0)
           := by
             -- This is true by distributivity of finite sums over addition
             rw [Finset.sum_add_distrib]
     rw [h]
-    /- Expand the LHS using clickedValue_symmDiff
-    (clickedValue S1 v + ∑ u, if u~v then (clickedValue S1 u) else 0)
-      + (clickedValue S2 v + ∑ u, if u~v then (clickedValue S2 u) else 0)
+    /- Expand the LHS using pressedValue_symmDiff
+    (pressedValue S1 v + ∑ u, if u~v then (pressedValue S1 u) else 0)
+      + (pressedValue S2 v + ∑ u, if u~v then (pressedValue S2 u) else 0)
     = (changedValue G S1 v) + (changedValue G S2 v)
     By associativity and commutativity of addition
     -/
@@ -174,17 +174,19 @@ Lights Out operation on state vector x.
 For each vertex v, its value is the sum in 𝔽₂ of x at v
 and the values of x at all verticies adjacent to v
 -/
-def phiVec {V : Type*} [Fintype V] [DecidableEq V]
+def phiVec
+  {V : Type*} [Fintype V] [DecidableEq V]
   (G : SimpleGraph V) [DecidableRel G.Adj]
   (x : V → ZMod 2)
   : V → ZMod 2
   := fun v => x v + ∑ u : V, if G.Adj v u then x u else 0
 
-/- Φ G tells us, for a simple graph G, if some vertices are clicked
+/- Φ G tells us, for a simple graph G, if some vertices are pressed
 which vertices will change state.
 The definition also proves that Φ G is linear.
 -/
-def Φ {V : Type*} [Fintype V] [DecidableEq V]
+def Φ
+  {V : Type*} [Fintype V] [DecidableEq V]
   (G : SimpleGraph V) [DecidableRel G.Adj] :
   (V → ZMod 2) →ₗ[ZMod 2] (V → ZMod 2)
   := {
@@ -261,12 +263,12 @@ def Φ {V : Type*} [Fintype V] [DecidableEq V]
       rw [h, smul_add]
   }
 
--- Φ and changedValue represent the same concept: which vertices change when some are clicked
-theorem phi_clickedValue_eq_changedValue
+-- Φ and changedValue represent the same concept: which vertices change state when some are pressed
+theorem phi_pressedValue_eq_changedValue
   {V : Type*} [Fintype V] [DecidableEq V]
   (G : SimpleGraph V) [DecidableRel G.Adj]
   (S : State V)
-  : Φ G (clickedValue S) = changedValue G S
+  : Φ G (pressedValue S) = changedValue G S
   := by rfl
 
 -- Φ and phiVec are the same function
@@ -277,10 +279,11 @@ theorem phi_phiVec
   : Φ G x = phiVec G x
   := by rfl
 
-/- Given a simple graph G and set of clicked vertices S,
+/- Given a simple graph G and set of pressed vertices S,
 Give back the set of vertices that change state.
 -/
-def phiSet {V : Type*} [Fintype V] [DecidableEq V]
+def phiSet
+  {V : Type*} [Fintype V] [DecidableEq V]
   (G : SimpleGraph V) [DecidableRel G.Adj]
   (S : State V)
   : State V
@@ -300,34 +303,21 @@ lemma phiSet_symmDiff
     rcases zmod2_cases (changedValue G S2 v) with h2 | h2 <;>
     simp [h1, h2]
 
--- Φ and phiSet represent the same concept: which verticies change when some are clicked
-theorem phi_clickedValue_eq_phiSet
+-- Φ and phiSet represent the same concept: which verticies change when some are pressed
+theorem phi_pressedValue_eq_phiSet
   {V : Type*} [Fintype V] [DecidableEq V]
   (G : SimpleGraph V) [DecidableRel G.Adj]
   (S : State V)
-  : Φ G (clickedValue S) = clickedValue (phiSet G S)
+  : Φ G (pressedValue S) = pressedValue (phiSet G S)
   := by
     funext v
-    rw [phi_clickedValue_eq_changedValue]
-    simp only [phiSet, clickedValue, Finset.mem_filter]
+    rw [phi_pressedValue_eq_changedValue]
+    simp only [phiSet, pressedValue, Finset.mem_filter]
     rcases zmod2_cases (changedValue G S v) with h | h
     · simp [h]
     · simp [h]
 
-/- With our theorems so far, we proved the following diagram commutes
-
-State V -----clickedValue-----> V → ZMod 2
-  |                                 |
-  |                                 |
-phiSet G                        Φ G OR phiVec G
-  |                                 |
-  |                                 |
-  V                                 V
-State V -----clickedValue-----> V → ZMod 2
--/
-
-
--- Clicking a single vertex changes everything in the closed neighborhood
+-- Pressing a single vertex changes everything in the closed neighborhood
 lemma phiSet_singleton
   {V : Type*} [Fintype V] [DecidableEq V]
   (G : SimpleGraph V) [DecidableRel G.Adj]
@@ -337,7 +327,7 @@ lemma phiSet_singleton
     -- For every u ∈ V, we'll show u ∈ (phiSet G {v}) iff u ∈ (closedNeighborhood G v)
     ext u
     -- Our goal is equivalent to "u changes when we press v" iff "u=v or u~v"
-    simp only [phiSet, closedNeighborhood, changedValue, clickedValue,
+    simp only [phiSet, closedNeighborhood, changedValue, pressedValue,
       true_and, Finset.mem_univ, Finset.mem_filter, Finset.mem_singleton]
     -- Need to show ∑ x, [u~v]*[x=v] = u~v
     have hsum :
@@ -370,11 +360,11 @@ lemma phiSet_singleton
         Decidable.not_not, zero_add, false_or, ite_false, ite_eq_left_iff, zero_ne_one, imp_false]
       exact G.adj_comm u v
 
-/- Clicking a sequence of verticies is the same as just
-pressing the ones clicked an odd number of times.
-The order of clicks also doesn't matter.
+/- Pressing a sequence of verticies is the same as just
+pressing the ones pressed an odd number of times.
+The order of presses also doesn't matter.
 -/
-theorem pressSequence_eq_pressedSet
+theorem pressSequence_eq_phiSet_pressedSet
   {V : Type*} [Fintype V] [DecidableEq V]
   (G : SimpleGraph V) [DecidableRel G.Adj]
   (S : State V)
@@ -386,7 +376,7 @@ theorem pressSequence_eq_pressedSet
     | nil =>
       have hphi : phiSet G (∅ : State V) = ∅ := by
         ext u
-        simp [phiSet, changedValue, clickedValue]
+        simp [phiSet, changedValue, pressedValue]
       simp [pressSequence, pressedSet, hphi, Finset.symmDiff_def]
     -- Assume that pressing list vs satisfies our goal (ih).
     -- If we press v and then vs, our goal is also satisfied
@@ -395,7 +385,34 @@ theorem pressSequence_eq_pressedSet
       simp only [pressSequence, pressedSet]
       rw [ih (press G S v), press, phiSet_symmDiff, phiSet_singleton, symmDiff_assoc]
 
-#exit
+/- With our theorems so far, we proved the following diagram commutes
+
+    *--------> State V -----pressedValue-----> V → ZMod 2
+    |            |                                  |
+    |            |                                  |
+    |         phiSet G                             Φ G
+pressedSet       |                                  |
+    |            |                                  |
+    |            V                                  V
+    |          State V -----pressedValue-----> V → ZMod 2
+    |            ^
+    |            |
+    |            |
+    |       pressSequence G ∅
+    |            |
+    |            |
+    *--------- List V
+
+* Starting from an empty initial state, pressSequence and phiSet both tell us
+  which verticies changed state, given the vertices pressed.
+* Thus instead of a sequence of presses, we can think about
+  the set of buttons were pressed an odd number of times.
+* We can also think of the state as a vector, with values in 𝔽₂.
+  Then Φ G describes the same transformation, and that transofmration is linear.
+
+Now that we have linear transformations over vectors, we can introduce linear algebra concepts.
+Most importantly, we can look at the kernel and its dimension.
+-/
 
 -- nullity(G) = rank ker Φ G
 noncomputable def nullity
